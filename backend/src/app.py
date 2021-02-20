27467675e02bf0966d76234cd84a614765ba624f
Victor Samuel <victor.samuelsantoss@gmail.com>
2021-02-20 16:05:35 -0300

    Implementaçao da rota de busca por nome

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,11 +1,23 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route('/search/<name>')
 def search_name(name):
-    return f'{name}'
+
+    try:
+        search = name
+        db = open ('src/allgames.txt')
+
+        for line in db:
+            if search in line:
+                return json.loads(line)
+    
+    except IOError as error: 
+        return f"Message: {error}"
 
 @app.route('/graph/<appid>')
 def get_graph(appid):
     return appid
+
