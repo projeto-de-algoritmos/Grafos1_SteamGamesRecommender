bb79bcf948107b4f095ed01efc318a5e2b052273
Wagner Martins <wagnermc506@gmail.com>
2021-02-20 17:29:03 -0300

    Atualizaçao do grafo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,22 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import Graph from 'react-vis-network-graph';
+import AsyncSelect from 'react-select/async';
+
+import axios from 'axios';
 
 function App() {
 
-  const graph = {
+  const api = axios.create({
+    baseURL: "localhost:5000"
+  })
+  const [selected, setSelected] = useState();
+  // const [appid, setAppId] = useState();
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [network, setNetwork] = useState();
+
+  const [graph, setGraph] = useState({
     nodes: [
       {id: 25484, label: "teste"},
       {id: 123, label: "teste2"},
@@ -17,8 +29,8 @@ function App() {
       {from: 123, to: 85184},
       {from: 85184, to: 25484}
     ]
-  }
-  
+  });
+
   const options = {
     edges: {
       arrows: {
@@ -34,11 +46,57 @@ function App() {
     width: '75%'
   }
 
+  const loadOptions = (inputvalue, callback) => {
+    // api.get(`/search/${inputvalue}`).then(
+    //   (Response) => {
+    //     // tratar Response
+    //     callback(Response)
+    //   }
+    // )
+    setTimeout(() => {
+      callback([
+        {value: "abc", label: "teste"}
+      ])
+    }, 1000)
+  }
+
+  const handleSelection = (e) => {
+    setSelected(e.value)
+  }
+
+  const inputChange = (value) => {
+    setName(value);
+  }
+
+  const submit = () => {
+    // api.get(`/${selected}`).then(
+    //   (Response) => {
+    //     setGraph(Response)
+    //   }
+    // )
+
+    // setGraph({})
+    
+    network.setData({
+      nodes: [
+        {id: 1, label: "a"},
+        {id: 2, label: "b"},
+        {id: 3, label: "c"}
+      ],
+      edges: [
+        {from: 1, to: 2},
+        {from: 3, to: 1},
+      ]})
+  }
+
   return (
     <div id="all">
       <div id="Header">SteamGamesRecommender</div>
-      <div id="Search"></div>
-      <Graph graph={graph} options={options} style={style}/>
+      <div id="Search">
+        <AsyncSelect cacheOptions loadOptions={loadOptions} onInputChange={inputChange} onChange={handleSelection}/>
+        <button onClick={submit}>Search</button>
+      </div>
+      <Graph graph={graph} options={options} style={style} getNetwork={network => setNetwork(network)}/>
     </div>
   );
 }
